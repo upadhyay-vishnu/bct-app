9ed9d457763c581d06f0e4be2e9ae7d112ca6f34
nikhil1904 <nikhilanandani@Nikhils-MacBook-Pro.local>
2024-06-26 18:21:20 +0530

    init commit with boilerplate

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+# models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+def root():
+    return {"message": "welcome to radar360 backend app"}
